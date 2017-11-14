cc619d2b0de7cec7d6a1cf89ac304451f70e5cdc
FelixvL <digitaleservice@gmail.com>
2017-11-14 16:06:05 +0100

    insert teams werkt

--- teams.php
+++ teams.php
@@ -1,3 +1,29 @@
 <?php
+    $hostname='localhost';
+    $databasenaam='sport_pool';
+    $username='root';
+    $password='';
+    
+    $conn = new mysqli($hostname, $username,$password,$databasenaam);
+    if(isset($_GET['naam'])){
+        $sql = "INSERT INTO `elftal`(`naam`, `plaats`)VALUES('".$_GET['naam']."','".$_GET['plaats']."')";
+        $conn->query($sql);
+    }
+ ?>
 
-echo "teams";
+
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <a href="index.php">terug</a>
+        <form action="teams.php" method="get">
+            Naam:<input type="text" name="naam">
+            <br>
+            Plaats:<input type="text" name="plaats">
+            <br>
+            <input type="submit" value="voeg toe">
+        </form>
+    </body>
+</html>
