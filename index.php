534fe6762dc9ac48b50db6da0b5f3616fa36e44d
FelixvL <digitaleservice@gmail.com>
2017-11-14 16:28:50 +0100

    select werkt per team

--- index.php
+++ index.php
@@ -5,5 +5,35 @@
     </head>
     <body>
         <a href="teams.php">maak teams aan</a>
+
+<?php
+
+    $hostname='localhost';
+    $databasenaam='sport_pool';
+    $username='root';
+    $password='';
+    
+    $conn = new mysqli($hostname, $username, $password, $databasenaam);
+    $sql = "SELECT * FROM `elftal`;";
+    $result = $conn->query($sql);
+    echo "<select>";
+    for($x = 0; $x < $result->num_rows; $x++){
+        $row = $result->fetch_assoc();
+        echo "<option>";
+        echo $row['naam'];
+        echo "</option>";
+    }
+    echo "</select>";
+        
+ //       <select>
+ //           <option>a</option>
+ //           <option>b</option>
+ //       </select>
+
+?>
+
+
+
+
     </body>
 </html>
