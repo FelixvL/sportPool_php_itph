a7be73809d8d0a2344d1af108dbe40a0dd958a0f
ahtec <gerarddoets@gmail.com>
2017-11-15 13:26:33 +0100

    button team php gestuled  en in css

--- teams.php
+++ teams.php
@@ -1,24 +1,28 @@
 <?php
-    $hostname='localhost';
-    $databasenaam='sport_pool';
-    $username='root';
-    $password='';
-    
-    $conn = new mysqli($hostname, $username,$password,$databasenaam);
-    if(isset($_GET['naam'])){
-        $sql = "INSERT INTO `elftal`(`naam`, `plaats`)VALUES('".$_GET['naam']."','".$_GET['plaats']."')";
-        $conn->query($sql);
-    }
- ?>
+$hostname = 'localhost';
+$databasenaam = 'sport_pool';
+$username = 'root';
+$password = '';
+
+$conn = new mysqli($hostname, $username, $password, $databasenaam);
+if (isset($_GET['naam'])) {
+    $sql = "INSERT INTO `elftal`(`naam`, `plaats`)VALUES('" . $_GET['naam'] . "','" . $_GET['plaats'] . "')";
+    $conn->query($sql);
+}
+?>
 
 
 <html>
     <head>
-        
+        <link rel = "stylesheet" type = "text/css" href="SportPool.css">  
     </head>
     <body>
-        <a href="index.php">terug</a>
-        <form action="teams.php" method="get">
+        <!--<a href="index.php">terug</a>-->
+        <form action="index.php" method="get">
+            <button type=submit value="teams"  >  terug </button>
+        </form>
+
+        <form action="teams.php" method="get"  >
             Naam:<input type="text" name="naam">
             <br>
             Plaats:<input type="text" name="plaats">
